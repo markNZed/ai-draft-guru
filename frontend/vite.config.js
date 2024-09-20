a15c28dc60f180e8c7638119231f41a219dbbc95
Mark Hampton <mark.hampton@ieee.org>
2024-09-20 12:09:34 +0200

    rebase

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,14 +12,27 @@ export default defineConfig({
   server: {
     host: "::",
     port: "8080",
-  },
-  base: "/",
-  proxy: {
-    '/api': {
-      target: 'http://localhost:3001',
-      changeOrigin: true,
-      rewrite: (path) => path.replace(/^\/api/, ''),
-    },
+    base: "/",
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',  // Your backend URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
+        secure: false,
+      },
+    }
   },
   plugins: [react()],
   resolve: {
