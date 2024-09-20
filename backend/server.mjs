1e059780c404114015d687dd366df78739261b48
Mark Hampton <mark.hampton@ieee.org>
2024-09-20 12:47:56 +0200

    origin

--- backend/server.mjs
+++ backend/server.mjs
@@ -47,7 +47,7 @@ app.use(cors({
     // Allow requests with no origin (like mobile apps or curl requests)
     if(!origin) return callback(null, true);
     if(allowedOrigins.indexOf(origin) === -1){
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      const msg = `The CORS policy for this site does not allow access from the specified Origin not ${origin}`;
       return callback(new Error(msg), false);
     }
     return callback(null, true);
