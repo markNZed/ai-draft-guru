cf3df2a9d0208a969cc90b5c91707a90529a2e91
Mark Hampton <mark.hampton@ieee.org>
2024-09-21 09:53:52 +0200

    Remove babel from backend

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,9 +1,6 @@
 // jest.config.js
 
 export default {
-  transform: {
-    "^.+\\.mjs$": "babel-jest", // Use Babel to transform .mjs files
-  },
   moduleFileExtensions: ['js', 'mjs'], // Ensure Jest handles both .js and .mjs
   testEnvironment: 'node', // Ensure you're testing in a Node.js environment
 };
